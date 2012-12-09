f4c53d1554a72956d5cea30551a93be78d979e8f
Mark Kiefert <mkiefert@gmail.com>
2012-12-09 15:47:12 -0500

    KPI table

--- public/assets/js/queries.js
+++ public/assets/js/queries.js
@@ -1,3 +1,5 @@
 var conversionsPerMonth = " data := //ancestry totClicksMonth := solve 'month {month:'month, totClicks: sum(data.Clicks where data.month='month)} totClicksMonth "
 
 var top5PlacementsByConv = "data := //ancestry dataWithTotConv := data with {totConv:data.\"Total Registrations\" + data.\"View Signups\"} conversionsByPlacement := solve 'PlacementGroup {\"Placement Group\": 'PlacementGroup, Totconversions: sum(dataWithTotConv.totConv where dataWithTotConv.\"Placement Group\" = 'PlacementGroup)} rank := std::stats::rank(neg conversionsByPlacement.Totconversions) conversionsByPlacement where rank <= 5"
+
+var costPerConversion = "data := //ancestry dataWithTotConv := data with {totConv:data.\"Total Registrations\" + data.\"View Signups\"} totConversions := sum(dataWithTotConv.totConv) totMediaCost := sum(dataWithTotConv.\"Media Cost\") totMediaCost/totConversions"
