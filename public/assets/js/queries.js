c6bf22815c5157a37aa6455170fc49e830fb0b06
Mark Kiefert <mkiefert@gmail.com>
2012-12-09 17:38:09 -0500

    Conversion Histogram

--- public/assets/js/queries.js
+++ public/assets/js/queries.js
@@ -3,3 +3,5 @@ var conversionsPerMonth = " data := //ancestry totClicksMonth := solve 'month {m
 var top5PlacementsByConv = "data := //ancestry dataWithTotConv := data with {totConv:data.\"Total Registrations\" + data.\"View Signups\"} conversionsByPlacement := solve 'PlacementGroup {\"Placement Group\": 'PlacementGroup, Totconversions: sum(dataWithTotConv.totConv where dataWithTotConv.\"Placement Group\" = 'PlacementGroup)} rank := std::stats::rank(neg conversionsByPlacement.Totconversions) conversionsByPlacement where rank <= 5"
 
 var costPerConversion = "data := //ancestry dataWithTotConv := data with {totConv:data.\"Total Registrations\" + data.\"View Signups\"} totConversions := sum(dataWithTotConv.totConv) totMediaCost := sum(dataWithTotConv.\"Media Cost\") totMediaCost/totConversions"
+
+var interactionHisto = "data := //Conversions/Interaction interactionCounts := solve 'id {\"id\": 'id, interactions: max(data.interactionNum where data.id = 'id)} histInteractions := solve 'interactions {interactions: 'interactions, conversions: count(interactionCounts.interactions where interactionCounts.interactions = 'interactions)} histInteractions"
