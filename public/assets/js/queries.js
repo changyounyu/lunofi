5503a322f95a804291fa888b062abf64e78c6072
Mark Kiefert <mkiefert@gmail.com>
2012-12-09 11:59:02 -0500

    Top Performers Leaderboard

--- public/assets/js/queries.js
+++ public/assets/js/queries.js
@@ -1,1 +1,3 @@
 var theQuery = " data := //ancestry totClicksMonth := solve 'month {month:'month, totClicks: sum(data.Clicks where data.month='month)} totClicksMonth "
+var top5PlacementsByConv = "data := //ancestry dataWithTotConv := data with {totConv:data.\"Total Registrations\" + data.\"View Signups\"} conversionsByPlacement := solve 'PlacementGroup {\"Placement Group\": 'PlacementGroup, Totconversions: sum(dataWithTotConv.totConv where dataWithTotConv.\"Placement Group\" = 'PlacementGroup)} rank := std::stats::rank(neg conversionsByPlacement.Totconversions) conversionsByPlacement where rank <= 5"
+
