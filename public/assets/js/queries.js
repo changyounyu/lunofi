5f4e448beffca49cb6ea9ed2e21f48d0a4b45e98
Scott McFarland <smcfarland@gmail.com>
2012-12-09 11:53:29 -0500

    renamed query for conversions per month

--- public/assets/js/queries.js
+++ public/assets/js/queries.js
@@ -1,1 +1,1 @@
-var theQuery = " data := //ancestry totClicksMonth := solve 'month {month:'month, totClicks: sum(data.Clicks where data.month='month)} totClicksMonth "
+var conversionsPerMonth = " data := //ancestry totClicksMonth := solve 'month {month:'month, totClicks: sum(data.Clicks where data.month='month)} totClicksMonth "
